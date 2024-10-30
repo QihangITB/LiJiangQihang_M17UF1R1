fdb46f402b3ea5055e92c2b54b1bab0ca8b58c39
QihangITB <qihang.li.7e7@itb.cat>
2024-10-30 02:36:51 +0100

    improve UI scene but need to implement transition

--- VVVVV_Qihang/Assets/Scripts/GameManager.cs
+++ VVVVV_Qihang/Assets/Scripts/GameManager.cs
@@ -1,37 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 //Todo el tema de ajustes del juego.
 public class GameManager : MonoBehaviour
 {
+    const string MainScene = "MainMenu";
     const string TutorialScene = "LevelTutorial";
+    const string PauseScene = "PauseMenu";
+    const string GameOverScene = "GameOverMenu";
+    const string GameplayScene = "Level";
+
     public static GameManager manager;
 
     public void Awake()
     {
         if(manager != null && manager != this )
+        {
             Destroy(this.gameObject);
+        }
+        else
+        {
+            manager = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
 
-        manager = this;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name.Contains(GameplayScene))
+            SwitchPause();
+    }
+
+
+    public void SwitchPause()
+    {
+        if(!SceneManager.GetSceneByName(PauseScene).isLoaded)
+            Pause();
+        else
+            Resume();
     }
 
     public void Play()
     {
         Debug.Log("Play");
+
+        //Cargamos la escena de juego.
         SceneManager.LoadScene(TutorialScene);
     }
 
     public void Quit()
     {
         Debug.Log("Quit");
+
+        //Cerramos la aplicación.
         Application.Quit();
     }
 
     public void Pause()
     {
         Debug.Log("Pause");
+        if (!SceneManager.GetSceneByName(PauseScene).isLoaded)
+        {
+            Time.timeScale = 0f; //Pausamos el juego.
+            SceneManager.LoadScene(PauseScene, LoadSceneMode.Additive);
+        }
     }
+ 
+    public void Resume()
+    {
+        Debug.Log("Resume");
+        Time.timeScale = 1f; //Reanudamos el juego.
+        SceneManager.UnloadSceneAsync(PauseScene);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GameOver");
+
+        SceneManager.LoadScene(GameOverScene);
+    }
+
+    public void Home()
+    {
+        Debug.Log("Home");
+
+        //Cargamos la escena del inicio.
+        SceneManager.LoadScene(MainScene);
+    }
+
 
 }
